b5c0509768f5781606b87f3f6bd544fce7eef1f3
libetc95 <151031212+libetc95@users.noreply.github.com>
2023-11-16 09:12:41 -0300

    Add files via upload

---
+++ subir.js
@@ -0,0 +1,15 @@
+<script type='text/javascript'>
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+</script>
